a8614bc8d0ff64b5952fd125f2515767b8564843
gdhwmik <84056782+gdhwmik@users.noreply.github.com>
2021-07-21 21:27:41 +0800

    Update temperature.c

--- temperature.c
+++ temperature.c
@@ -15,6 +15,124 @@
 #define HWMON_DEV "hwmon0"
 #define HWMON_TEMP "temp1_input"
 
+
+int gpio_direction(int gpio, int dir)
+{
+	int ret = 0;
+	char buf[50];
+
+	sprintf(buf, "/sys/class/gpio/gpio%d/direction", gpio);
+	int gpiofd = open(buf, O_WRONLY);
+	if (gpiofd < 0) {
+		perror("Couldn't open IRQ file");
+		ret = -1;
+	}
+
+	if (dir == 2 && gpiofd){
+		if (3 != write(gpiofd, "high", 3)) {
+			perror("Couldn't set GPIO direction to out");
+			ret = -2;
+		}
+	}
+
+	if (dir == 1 && gpiofd){
+		if (3 != write(gpiofd, "out", 3)) {
+			perror("Couldn't set GPIO direction to out");
+			ret = -3;
+		}
+	} else if (gpiofd) {
+		if(2 != write(gpiofd, "in", 2)) {
+			perror("Couldn't set GPIO directio to in");
+			ret = -4;
+		}
+	}
+
+	close(gpiofd);
+	return ret;
+}
+
+int gpio_export(int gpio)
+{
+	int efd;
+	char buf[50];
+	int gpiofd, ret;
+
+	/* Quick test if it has already been exported */
+	sprintf(buf, "/sys/class/gpio/gpio%d/value", gpio);
+	efd = open(buf, O_WRONLY);
+	if (efd != -1) {
+		close(efd);
+		return 0;
+	}
+
+	efd = open("/sys/class/gpio/export", O_WRONLY);
+	if (efd != -1) {
+		sprintf(buf, "%d", gpio);
+		ret = write(efd, buf, strlen(buf));
+		if(ret < 0) {
+			perror("Export failed");
+			return -2;
+		}
+		close(efd);
+	} else {
+		// If we can't open the export file, we probably
+		// dont have any gpio permissions
+		return -1;
+	}
+
+	return 0;
+}
+
+int gpio_read(int gpio)
+{
+	char in[3] = {0, 0, 0};
+	char buf[50];
+	int nread, gpiofd;
+
+	sprintf(buf, "/sys/class/gpio/gpio%d/value", gpio);
+	gpiofd = open(buf, O_RDWR);
+	if (gpiofd < 0) {
+		fprintf(stderr, "Failed to open gpio %d value\n", gpio);
+		perror("gpio failed");
+	}
+
+	do {
+		nread = read(gpiofd, in, 1);
+	} while (nread == 0);
+
+	if (nread == -1){
+		perror("GPIO Read failed");
+		return -1;
+	}
+
+	close(gpiofd);
+	return atoi(in);
+}
+
+int gpio_write(int gpio, int val)
+{
+	char buf[50];
+	int nread, ret, gpiofd;
+
+	sprintf(buf, "/sys/class/gpio/gpio%d/value", gpio);  //此將此字串存入buf陣列
+	gpiofd = open(buf, O_RDWR);							 //打開value檔；buf只是提供路徑
+	if (gpiofd > 0) {
+		snprintf(buf, 2, "%d", val);					 //將兩個字元存入buf陣列(含結尾數字)，存入數據為%d值為起對此函式所傳過來的值
+		ret = write(gpiofd, buf, 2);					 //將buf內資料兩字元大小存入value檔(此為1＝HI)
+		if (ret < 0) {
+			perror("failed to set gpio");
+			return 1;
+		}
+
+		close(gpiofd);
+		if (ret == 2) return 0;							 //write()成功會還傳寫入位元組數
+	}
+
+	return 1;
+}
+
+
+
 struct sys_hwmon {
 	int dev;
 	char *hwmon_buf;
@@ -33,7 +151,8 @@ int main()
     char str_temp[10];
     int int_temp;
     size_t count;
-
+	int gpio_on, gpio_off, i;
+	
     con_hwmon_temp.dev = 0;
     con_hwmon_temp.hwmon_buf = &hwmon_con_buf[0];
 
@@ -64,20 +183,42 @@ int main()
 			exit(1);
 		}
 
-/*		i = 0;
-		while (1) {
-			if (str_temp[i] == 0x0a) {
-				str_temp[i] = 0;
-				str_temp[i + 1] = 0;
-				break;
-			}
-			i++;
-		}
-*/		int_temp = atoi(str_temp);
+		int_temp = atoi(str_temp);
 		printf("Temp %d\r\n", int_temp);
 
 		close(fd);
-		usleep(1000000);
+		
+/*		"Simple gpio access\n"
+	  "\n"
+	  "  -h, --help              This message\n"
+	  "  -p, --getin <dio>       Returns the input value of n sysfs DIO\n"
+	  "  -e, --setout <dio>      Sets a sysfs DIO output value high\n"
+	  "  -l, --clrout <dio>      Sets a sysfs DIO output value low\n"
+	  "  -d, --ddrout <dio>      Set sysfs DIO to an output\n"
+	  "  -w, --waitfor <dio>     Wait for IO to change to the configured edge\n"
+	  "  -r, --ddrin <dio>       Set sysfs DIO to an input\n\n",
+*/
+		if (int_temp > 33900)
+		{
+			gpio_on = 33;
+			gpio_export(gpio_on);
+			gpio_direction(gpio_on, 1);
+			gpio_write(gpio_on, 0);
+			gpio_off = 32;
+			gpio_export(gpio_off);
+			gpio_write(gpio_on, 1);
+		}
+		else if (int_temp < 33850)
+		{
+			gpio_on = 32;
+			gpio_export(gpio_on);
+			gpio_direction(gpio_on, 1);
+			gpio_write(gpio_on, 0);
+			gpio_off = 33;
+			gpio_export(gpio_off);
+			gpio_write(gpio_on, 1);
+		}
+		usleep(10000000);
 	};
 
     return 0;
