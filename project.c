cbdf07c05170cd4cb978b825b0e769ad8e28c73e
gdhwmik <84056782+gdhwmik@users.noreply.github.com>
2021-07-21 16:10:35 +0800

    Update project.c

--- project.c
+++ project.c
@@ -6,21 +6,22 @@
 #include <sys/select.h>
 #include <sys/stat.h>
 
-int main()
+
+int temper_read(int void)
 {
-	char temper[3] = {0,0,0};
+	char in[3] = {0, 0, 0};
 	char buf[50];
 	int nread, hwmonfd;
 
 	sprintf(buf, "/sys/class/hwmon/hwmon0/temp1_input");
 	hwmonfd = open(buf, O_RDWR);
-	if (hwmonfd < 0) {
-		fprintf(stderr, "Failed to open hwmon0 temp1\n");
+	if (gpiofd < 0) {
+		fprintf(stderr, "Failed to open hwmonfd\n");
 		perror("hwmon failed");
 	}
 
 	do {
-		nread = read(hwmonfd, temper, 1);
+		nread = read(hwmonfd, in, 1);
 	} while (nread == 0);
 
 	if (nread == -1){
@@ -29,7 +30,17 @@ int main()
 	}
 
 	close(hwmonfd);
-	printf("Temperature is %d",temper);
-	
-	return 0;
+	return atoi(in);
 }
+
+int main()
+{
+	int temper, i;
+	printf("hello world");
+	printf("Temperature is %d\n", temper_read());
+	
+	return 0;		
+}
+
+
+
