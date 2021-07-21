78453bdf44a8905a3285e7a2b0ff43de36fe7af7
gdhwmik <84056782+gdhwmik@users.noreply.github.com>
2021-07-21 12:18:18 +0800

    Update project.c

--- project.c
+++ project.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <sys/stat.h>
 
+int main()
+{
+	int temper;
+	char buf[50];
+	int nread, hwmonfd;
+
+	buf = "/sys/class/hwmon/hwmon0/temp1_input" ;
+	hwmonfd = open(buf, O_RDWR);
+	if (hwmonfd < 0) {
+		fprintf(stderr, "Failed to open gpio %d value\n", gpio);
+		perror("gpio failed");
+	}
+
+	do {
+		nread = read(gpiofd, in, 1);
+	} while (nread == 0);
+
+	if (nread == -1){
+		perror("GPIO Read failed");
+		return -1;
+	}
+
+	close(gpiofd);
+	return atoi(in);
