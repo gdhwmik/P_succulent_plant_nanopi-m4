a1e3dc00ba566c56a2274bd3ff84aaece253c440
gdhwmik <84056782+gdhwmik@users.noreply.github.com>
2021-07-25 20:40:37 +0800

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,16 +1,14 @@
 import cv2
 import numpy as np
-cv2.namedWindow('testcamera')
-capture = cv2.VideoCapture(0)  #'0'代表从摄像头读入，'./video.avi'代表读入视频
-capture.isOpened()
-num = 0
-while 1:
-    ret, frame = capture.read()
-    cv2.imshow('testcamera', frame)
-    key = cv2.waitKey(1)
-    num += 1
-    if key == 1048603:  # <ESC>
-        break
+import sys
 
-capture.release()
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("hsv_demo", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+cap.release()
 cv2.destroyAllWindows()
