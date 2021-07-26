6b746c45fbe7e4223b85998de8598b8d8a14b58b
gdhwmik <84056782+gdhwmik@users.noreply.github.com>
2021-07-26 10:35:40 +0800

    Update videocapture.py

--- videocapture.py
+++ videocapture.py
@@ -8,26 +8,26 @@
 
 import cv2  # import opencv
 
-# use opencv open the video 1.mp4
-videoCapture = cv2.VideoCapture("/dev/video10")
+# use opencv open the /dev/video10
+cap = cv2.VideoCapture("/dev/video10")
 # get the inf of vedio,fps and size
-fps = videoCapture.get(cv2.CAP_PROP_FPS)
-size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
-        int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+fps = cap.get(cv2.CAP_PROP_FPS)
+size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
 # point out how to encode videos
 # I420-avi=>cv2.cv.CV_FOURCC('X','2','6','4');
 # MP4=>cv2.cv.CV_FOURCC('M', 'J', 'P', 'G')
 # The mp4 encoder in my computer do not work,so i just use X264
-videoWriter = cv2.VideoWriter('oto_other.avi', cv2.VideoWriter_fourcc('X', '2', '6', '4'), fps, size)
+videoWriter = cv2.VideoWriter('oto_other.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, size)
 # read one frame from the video
-success, frame = videoCapture.read()
+success, frame = cap.read()
 while success:
     cv2.imshow("Oto Video", frame)  # display this frame
-    cv2.waitKey(int(fps))  # delay
+    #cv2.waitKey(int(fps))  # delay
     videoWriter.write(frame)  # write one frame into the output video
-    success, frame = videoCapture.read()  # get the next frame of the video
+    success, frame = cap.read()  # get the next frame of the video
 # some process after finish all the program
 cv2.destroyAllWindows()     # close all the widows opened inside the program
-videoCapture.release        # release the video read/write handler
+cap.release        # release the video read/write handler
 videoWriter.release
