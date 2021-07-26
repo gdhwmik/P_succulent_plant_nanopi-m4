a67cb6d4cbc8e9bfabd7c87e97033dbc381fdb65
gdhwmik <84056782+gdhwmik@users.noreply.github.com>
2021-07-26 11:18:49 +0800

    Update videocapture.py

--- videocapture.py
+++ videocapture.py
@@ -19,14 +19,14 @@ size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
 # I420-avi=>cv2.cv.CV_FOURCC('X','2','6','4');
 # MP4=>cv2.cv.CV_FOURCC('M', 'J', 'P', 'G')
 # The mp4 encoder in my computer do not work,so i just use X264
-videoWriter = cv2.VideoWriter('oto_other.avi', cv2.VideoWriter_fourcc('X', '2', '6', '4'), fps, size)
+#videoWriter = cv2.VideoWriter('oto_other.avi', cv2.VideoWriter_fourcc('X', '2', '6', '4'), fps, size)
 # read one frame from the video
 success, frame = cap.read()
 
 while success:
     cv2.imshow("Oto Video", frame)  # display this frame
     cv2.waitKey(int(fps))  # delay
-    videoWriter.write(frame)  # write one frame into the output video
+#    videoWriter.write(frame)  # write one frame into the output video
     success, frame = cap.read()  # get the next frame of the video
 # some process after finish all the program
 cv2.destroyAllWindows()     # close all the widows opened inside the program
