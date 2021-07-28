c84ece5427b928d56ee459538778ef724d3b83fc
gdhwmik <84056782+gdhwmik@users.noreply.github.com>
2021-07-28 22:34:10 +0800

    Update open_script.sh

--- open_script.sh
+++ open_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/pi/Project_succulent_plant./environment_control &
-python3 /home/pi/Project_succulent_plant/test_succulent_plant_pitc.py 
+/home/pi/P_succulent_plant_nanopi-m4/./environment_control &
+python3 /home/pi/P_succulent_plant_nanopi-m4/test_succulent_plant_pitc.py 
